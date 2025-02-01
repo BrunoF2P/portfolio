09966835705f30ae3efcd2285660e3ecf4109c99
Bruno Bispo <brunobispof2p@gmail.com>
2025-02-01 16:24:21 -0300

    vercel adapter for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     icon()
   ],
+
   i18n: {
     locales: ['en', 'pt-br'],
     defaultLocale: "en",
@@ -14,4 +16,7 @@ export default defineConfig({
       prefixDefaultLocale: true
   }
   },
+  
+  output: 'server',
+  adapter: vercel(),
 });
